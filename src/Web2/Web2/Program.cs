d6934f58a2c0fbf5573248d8509b152d9168e665
Ares25123 <milutin25a@gmail.com>
2024-01-27 15:00:03 +0300

    а

--- src/Web2/Web2/Program.cs
+++ src/Web2/Web2/Program.cs
@@ -16,18 +16,18 @@ namespace BlazorWeb
                options => options.UseSqlServer(
                    "Server=Ares;Database=praktika;Trusted_Connection=True;MultipleActiveResultSets=true"));
 
-            // Add services to the container.
+          
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+         
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
 
